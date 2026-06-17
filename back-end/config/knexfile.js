2efc9ab3c656ff293056fb9719fdac394d7deb42
Santhosh <santhoshsakthi444@gmail.com>
2026-06-17 20:57:09 +0530

    "Add Neon SSL support"

--- back-end/config/knexfile.js
+++ back-end/config/knexfile.js
@@ -3,33 +3,27 @@ require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+const commonConnection = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+};
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10
-    }
+    connection: commonConnection
   },
+
   production: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 20
+      ...commonConnection,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   }
 };
